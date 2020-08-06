ab1b015f059dd3cf8070f2f71d42f3fac1236baa
Andy Agus <Redagus@gmail.com>
2020-08-06 18:11:32 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React from 'react'
+
+class App extends React.Component{
+	
+	state = {
+		loading: true,
+		person: null,
+	}
+
+	async componentDidMount(){
+		const url = "http://api.open-notify.org/astros.json"
+
+		const response = await fetch(url);
+		const data = await response.json();
+		this.setState({person: data["people"][1].name, loading: false});
+
+	}
+
+
+	render(){
+
+		if(this.state.loading){
+			return <div> Loading... </div>
+		}else{
+			return <div> {this.state.person} </div>
+		}
+	}
+}
+
+export default App 
